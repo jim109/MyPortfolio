986ede866053775afa23c20e6a4c20adfd2d0f0e
Jimmy Osma <josma109@gmail.com>
2023-03-23 20:24:33 -0500

    initial project

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     eventListeners();
+
+    darkMode();
 });
 
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode')
+    }else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode')
+        }else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode')
+    })
+}
 
 function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu');
- 
-    mobileMenu.addEventListener('click', navegacionResponsive)
- 
-    //muestra campos condicionales
-    const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
-    metodoContacto.forEach(input => input.addEventListener('click', mostrarMetodosContacto)); 
-    
- }
- 
- function navegacionResponsive() {
-     const navegacion = document.querySelector('.navegacion');
- 
-     // toggle si tiene la clase la agrega sino la quita
-     navegacion.classList.toggle('mostrar');
- }
+   const mobileMenu = document.querySelector('.mobile-menu');
+
+   mobileMenu.addEventListener('click', navegacionResponsive)
+
+   //muestra campos condicionales
+   const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
+   metodoContacto.forEach(input => input.addEventListener('click', mostrarMetodosContacto)); 
+   
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    // toggle si tiene la clase la agrega sino la quita
+    navegacion.classList.toggle('mostrar');
+}
